b86b11c3685e3f098ba6fc0e2e62d83d9b62ebe7
bhutianimukul <bhutianimukul@gmail.com>
2022-04-28 17:08:36 +0530

    cors

--- src/main/java/com/project/contacts/contacts/WebConfig.java
+++ src/main/java/com/project/contacts/contacts/WebConfig.java
@@ -2,10 +2,11 @@ package com.project.contacts.contacts;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
